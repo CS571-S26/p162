70e1cf9d6afdd3d201b76eb041e4970da50c7480
Esteban Onate <eonate@wisc.edu>
2026-04-18 15:38:12 -0500

    made json file with team info, improved team cards

--- src/TeamCard.jsx
+++ src/TeamCard.jsx
@@ -1,10 +1,33 @@
 import { Card } from 'react-bootstrap'
+import { useState } from 'react'
 
 function TeamCard({ name }) {
+    const [showPlayers, setShowPlayers] = useState(false)
+
+    const handleClick = () => {
+        
+    }
+
   return (
-    <Card className="text-center shadow-sm">
+    <Card className="text-center shadow-sm" onClick={handleClick}>
       <Card.Body>
         <Card.Title>{name}</Card.Title>
+
+        <Button
+            variant="primary"
+            size="sm"
+            onClick={() => setShowPlayers(!showPlayers)}
+        >
+            {showPlayers ? "Hide Players" : "View Players"}
+        </Button>
+
+          {showPlayers && (
+            <ul className="mt-3">
+              {team.players.map((p, i) => (
+                <li key={i}>{p}</li>
+              ))}
+            </ul>
+          )}
       </Card.Body>
     </Card>
   )
--- src/pages/Teams.jsx
+++ src/pages/Teams.jsx
@@ -1,15 +1,34 @@
 import { Container, Col, Row, Card } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
 import TeamCard from '../TeamCard'
 
 function Teams() {
-  const teams = ["Karmine Corp", "Vitality", "Shopify Rebellion", "NRG", ]
+  const [teams, setTeams] = useState([])
+  const [region, setRegion] = useState("ALL")
+
+  useEffect(() => {
+    fetch("/p162/public/data/teams.json")
+      .then(res => res.json())
+      .then(data => setTeams(data))
+  }, [])
+
+  const filtered =
+    region === "ALL"
+      ? teams
+      : teams.filter(t => t.region === region)
 
   return (
     <Container className="mt-4">
       <h2 className="mb-4 text-center">Qualified Teams</h2>
 
+      <div className="text-center mb-4">
+        <Button onClick={() => setRegion("ALL")}>All</Button>{" "}
+        <Button onClick={() => setRegion("EU")}>EU</Button>{" "}
+        <Button onClick={() => setRegion("NA")}>NA</Button>
+      </div>
+
       <Row>
-        {teams.map((team, i) => (
+        {filtered.map((team, i) => (
           <Col xs={12} sm={6} md={3} key={i} className="mb-4">
             <TeamCard name={team}/>
           </Col>
