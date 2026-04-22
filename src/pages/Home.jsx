9361887cde2f26319d08cf1b06b9e43b9a1f87c9
Esteban Onate <eonate@wisc.edu>
2026-04-21 23:16:37 -0500

    implemented more tabs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Button, Container, Card, Tab, Tabs, Image } from 'react-bootstrap'
+import { Button, Container, Card, Tab, Tabs, Table } from 'react-bootstrap'
 import rlcsLogo from '../assets/RLCS_2020_darkmode.png'
 import m8swin from '../assets/m8swin.png'
 
@@ -29,16 +29,103 @@ function Home() {
           <Tab eventKey="overview" title="Overview">
             <h1>RLCS Paris Major featuring top teams worldwide.</h1>
             <img src={m8swin} width="400" height="270" alt="Fall 2026 Major Winners"/>
-            <p>The second major event of the RLCS 2026 season as these top 16 teams in the world seek to prove their skill at a major stage.</p>
-            <p>Along with prize pool, the teams are also competeting to acrue RLCS points to gain themselves qualification into the largest stage in Rocket League, the World Championship.</p>
+            <p>The second major event of the RLCS 2026 season as these top 16 teams in the world seek to prove their 
+              skill at a major stage with the goal of being major champions.
+            </p>
+            <p>Along with prize pool, the teams are also competeting to acrue RLCS points to gain themselves 
+              qualification into the largest stage in Rocket League, the World Championship.
+            </p>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Place</th>
+                  <th>RLCS Points</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>1st</th>
+                  <th>36</th>
+                </tr>
+                <tr>
+                  <th>2nd</th>
+                  <th>24</th>
+                </tr>
+                <tr>
+                  <th>3rd - 4th</th>
+                  <th>16</th>
+                </tr>
+                <tr>
+                  <th>5th - 6th</th>
+                  <th>12</th>
+                </tr>
+                <tr>
+                  <th>7th - 8th</th>
+                  <th>8</th>
+                </tr>
+                <tr>
+                  <th>9th - 12th</th>
+                  <th>6</th>
+                </tr>
+                <tr>
+                  <th>13th - 16th</th>
+                  <th>4</th>
+                </tr>
+              </tbody>
+            </Table>
           </Tab>
 
           <Tab eventKey="location" title="Location">
             <p>Paris La Défense Arena, France.</p>
+            <p>
+              Paris La Défense Arena is Europe's largest indoor multi-purpose venue, 
+              inaugurated in October 2017 in Nanterre, west of Paris. 
+              As the home of Racing 92 rugby club, it holds up to 30,681 spectators for matches and up to 45,000 for concerts. 
+              The arena, designed by Christian de Portzamparc, features a massive, modular, and technologically advanced design, 
+              and was home to various events in the 2024 Summer Olympics.
+            </p>
           </Tab>
 
           <Tab eventKey="prize" title="Prize Pool">
-            <p>$300,000 prize pool.</p>
+            <p>$300,000 prize pool distributed to all competitor.</p>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Place</th>
+                  <th>$ USD</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th>1st</th>
+                  <th>$102,000</th>
+                </tr>
+                <tr>
+                  <th>2nd</th>
+                  <th>$51,000</th>
+                </tr>
+                <tr>
+                  <th>3rd - 4th</th>
+                  <th>$36,000</th>
+                </tr>
+                <tr>
+                  <th>5th - 6th</th>
+                  <th>$22,500</th>
+                </tr>
+                <tr>
+                  <th>7th - 8th</th>
+                  <th>$15,000</th>
+                </tr>
+                <tr>
+                  <th>9th - 12th</th>
+                  <th>$9,000</th>
+                </tr>
+                <tr>
+                  <th>13th - 16th</th>
+                  <th>$4,500</th>
+                </tr>
+              </tbody>
+            </Table>
           </Tab>
         </Tabs>
       </Card>
